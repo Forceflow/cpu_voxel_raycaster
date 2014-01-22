bb31a8561c7bedf6509e992593104c5802b0c5e8
Jeroen Baert <jerbae@gmail.com>
2014-01-22 16:01:03 +0000

    Camera to struct, because lolz

--- voxel_raycaster/Camera.h
+++ voxel_raycaster/Camera.h
@@ -15,38 +15,24 @@
 
 using namespace trimesh;
 
-class Camera {
-public:
+struct Camera {
 	vec3 e_,g_,t_,u_,v_,w_;
-	Camera();
-	Camera(vec3 e, vec3 g, vec3 t);
-	//Eigen::Matrix4d getViewTransformation();
-	friend std::ostream& operator<< (std::ostream& o, Camera const& camera);
-	virtual ~Camera();
-	void computeUVW(vec3 e,vec3 g, vec3 t);
+
+	Camera() : e_(vec3(0, 0, 0)), g_(vec3(0, 0, -1)), t_(vec3(0, 1, 0)) {
+		this->computeUVW();
+	}
+
+	Camera(vec3 e, vec3 g, vec3 t) : e_(e), g_(g), t_(t) {
+		this->computeUVW();
+	}
+
+	void computeUVW(){
+		vec3 w = normalize(g_) * -1.0f;
+		vec3 u = normalize((vec3)(t_ CROSS w));
+		vec3 v = (w CROSS u);
+		w_ = w; u_ = u; v_ = v;
+	}
+
 };
-
-// Construct default camera at origin
-inline Camera::Camera() : e_(vec3(0,0,0)) , g_(vec3(0,0,-1)), t_(vec3(0,1,0)) {
-	computeUVW(e_,g_,t_);
-}
-
-// Construct a camera using, eye e, gaze g (the view direction), and view-up vector t (which way is up);
-inline Camera::Camera(vec3 e, vec3 g, vec3 t) : e_(e), g_(g), t_(t) {
-	this->computeUVW(e_,g_,t_);
-}
-
-// Private method to compute camera basis in UVW coordinates
-// This is like constructing the view matrix
-inline void Camera::computeUVW(vec3 e, vec3 g, vec3 t){
-	vec3 w = normalize(g) * -1.0f;
-	vec3 u = normalize((vec3) (t CROSS w));
-	vec3 v = (w CROSS u);
-	w_ = w; u_ = u; v_ = v;
-}
-
-inline Camera::~Camera() {
-	// TODO Auto-generated destructor stub
-}
 
 #endif /* CAMERA_H_ */
